006f2a435c15d6f4fd033116be60c02dba3a8e76
Nguyễn Hoàng Triều <chipmunk2811@yahoo.com>
2024-08-13 11:59:09 +0700

    waiting background english

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+// Change Language
+if (localStorage.getItem('language')) {
+    const storedData = localStorage.getItem('language');
+    switch (storedData) {
+        case 'lang-vi':
+            const english = document.getElementById("lang-eng");
+            if (english) english.id = 'lang-vi';
+
+            // Ẩn tất cả các phần tử ngôn ngữ
+            document.querySelectorAll('.lang').forEach(el => el.style.display = 'none');
+            // Hiển thị ngôn ngữ việt nam
+            document.querySelectorAll('.lang-vi').forEach(el => el.style.display = 'block');
+            document.querySelectorAll('.lang-vi-inline').forEach(el => el.style.display = 'inline');
+            break;
+        case 'lang-eng':
+            const vietnamese = document.getElementById("lang-vi");
+            if (vietnamese) vietnamese.id = 'lang-eng';
+            
+            // Ẩn tất cả các phần tử ngôn ngữ
+            document.querySelectorAll('.lang').forEach(el => el.style.display = 'none');
+            // Hiển thị ngôn ngữ tiếng anh
+            document.querySelectorAll('.lang-eng').forEach(el => el.style.display = 'block');
+            document.querySelectorAll('.lang-eng-inline').forEach(el => el.style.display = 'inline');
+            break;
+        default:
+            break;
+    }
+}
+
+document.getElementById('changeLang').onclick = () => {
+    const vietnamese = document.getElementById("lang-vi");
+    const english = document.getElementById("lang-eng");
+    if (vietnamese) {
+        vietnamese.id = "lang-eng"
+        // Ẩn tất cả các phần tử ngôn ngữ
+        document.querySelectorAll('.lang').forEach(el => el.style.display = 'none');
+        // Hiển thị ngôn ngữ tiếng anh
+        document.querySelectorAll('.lang-eng').forEach(el => el.style.display = 'block');
+        document.querySelectorAll('.lang-eng-inline').forEach(el => el.style.display = 'inline');
+        // Lưu ngôn ngữ đã chọn vào localStorage
+        localStorage.setItem('language', 'lang-eng')
+    }
+    if (english) {
+        english.id = "lang-vi"
+
+        // Ẩn tất cả các phần tử ngôn ngữ
+        document.querySelectorAll('.lang').forEach(el => el.style.display = 'none');
+        // Hiển thị ngôn ngữ việt nam
+        document.querySelectorAll('.lang-vi').forEach(el => el.style.display = 'block');
+        document.querySelectorAll('.lang-vi-inline').forEach(el => el.style.display = 'inline');
+        // Lưu ngôn ngữ đã chọn vào localStorage
+        localStorage.setItem('language', 'lang-vi')
+    }
+}
+
+// Chang Page
 const button = document.getElementById('changePage');
 const links = document.querySelectorAll('a');
 const spans = document.querySelectorAll('span');
@@ -14,12 +70,14 @@ button.addEventListener('click', function () {
     } else {
         button.value = 'open';
         links.forEach(link => {
-           
+
             link.classList.remove('hidden');
         });
         spans.forEach(span => {
-           
+
             span.classList.remove('hidden');
         });
     }
 });
+
+
